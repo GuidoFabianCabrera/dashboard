76f996d7fdf6a3b1fce24c9b8181a60f8417b30f
GuidoFC <guidofabiancabrera@hotmail.com>
2022-04-18 10:05:35 -0300

    login

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  middleware({ redirect }) {
+    return redirect("login");
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
